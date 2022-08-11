8c7a7dc2626d5f2f4fa6dcd2a75deb4c103d0d2a
Roberto Polli <robipolli@gmail.com>
2022-08-11 11:25:27 +0200

    Fix URL + tests

--- tests/test_urls.py
+++ tests/test_urls.py
@@ -1,43 +1,47 @@
-from pathlib import Path
 import re
-import requests
-import pytest
 import time
+from os import environ
+from pathlib import Path
 
-re_url = re.compile(r'[<"]https://github[^>"]*')
+import pytest
+import requests
+
+re_url = re.compile(r'[<"](https://github|https://raw.githubusercontent[^>"]*)[>"]')
+
 
 def get_urls(glob_path):
-  files = Path(".").glob(glob_path)
-  for f in files:
-    for url in re_url.findall(f.read_text()):
-        yield f, url.strip('<">')
+    files = Path(".").glob(glob_path)
+    for f in files:
+        for url in re_url.findall(f.read_text()):
+            if environ.get("PYTEST_ONLY", "") in url:
+                yield f, url.strip('<">')
 
 
-@pytest.mark.parametrize('fpath,url', get_urls("VocabolariControllati/**/*.ttl"))
-def test_vocab_url(fpath,url):
+@pytest.mark.parametrize("fpath,url", get_urls("VocabolariControllati/**/*.ttl"))
+def test_vocab_url(fpath, url):
     # raise NotImplementedError
-    if 'deprecated' in fpath.as_posix().lower():
+    if "deprecated" in fpath.as_posix().lower():
         return
-    if 'github.com/italia/daf-ontologie-vocabolari-controllati/issues' in url:
+    if "github.com/italia/daf-ontologie-vocabolari-controllati/issues" in url:
         return
 
     for i in range(3):
-      ret = requests.head(url)
-      if ret.status_code == 429:
-        time.sleep(int(ret.headers['Retry-After']))
-        continue
-    assert ret.status_code in (200,301,302)
+        ret = requests.head(url)
+        if ret.status_code == 429:
+            time.sleep(int(ret.headers["Retry-After"]))
+            continue
+    assert ret.status_code in (200, 301, 302)
 
 
-@pytest.mark.parametrize('fpath,url', get_urls("Ontologie/**/latest/*.ttl"))
-def test_onto_url(fpath,url):
-    if 'deprecated' in fpath.as_posix().lower():
+@pytest.mark.parametrize("fpath,url", get_urls("Ontologie/**/latest/*.ttl"))
+def test_onto_url(fpath, url):
+    if "deprecated" in fpath.as_posix().lower():
         return
-    if 'github.com/italia/daf-ontologie-vocabolari-controllati/issues' in url:
+    if "github.com/italia/daf-ontologie-vocabolari-controllati/issues" in url:
         return
     for i in range(3):
-      ret = requests.head(url)
-      if ret.status_code == 429:
-        time.sleep(int(ret.headers['Retry-After']))
-        continue
-    assert ret.status_code in (200,301,302)
+        ret = requests.head(url)
+        if ret.status_code == 429:
+            time.sleep(int(ret.headers["Retry-After"]))
+            continue
+    assert ret.status_code in (200, 301, 302)
